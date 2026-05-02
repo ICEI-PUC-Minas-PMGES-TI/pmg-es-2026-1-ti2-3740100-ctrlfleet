d1ccacaeb844bf84b7eee826332f3643361996f1
aalexiaandrade <alexiafernanda424@gmail.com>
2026-05-02 12:17:34 -0300

    feat: Criação da Entidade Veiculo

---
+++ src/back/src/main/java/com/ctrlfleet/api/StatusVeiculo.java
@@ -0,0 +1,8 @@
+package com.ctrlfleet.api;
+
+public enum StatusVeiculo {
+    DISPONIVEL,
+    EM_USO,
+    MANUTENCAO,
+    DESATIVADO
+}
---
+++ src/back/src/main/java/com/ctrlfleet/api/Veiculo.java
@@ -0,0 +1,77 @@
+package com.ctrlfleet.api;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Veiculos")
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "A placa é obrigatória")
+    @Column(unique = true, nullable = false, length = 10)
+    private String placa;
+
+    @NotBlank(message = "O modelo é obrigatório")
+    @Column(nullable = false)
+    private String modelo;
+
+    @NotBlank(message = "A marca é obrigatória")
+    @Column(nullable = false)
+    private String marca;
+
+    @NotNull(message = "O ano é obrigatório")
+    private int ano;
+
+    @Enumerated(EnumType.STRING) //Salva o nome do status no banco
+    @Column(nullable = false)
+    private StatusVeiculo status = StatusVeiculo.DISPONIVEL; // Define DISPONIVEL como valor padrão
+
+
+    public Veiculo(String placa, String modelo, String marca, int ano) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+    }
+
+    //ID
+    public Long getId() { 
+        return id; }
+    public void setId(Long id) { 
+        this.id = id; }
+
+    //PLACA
+    public String getPlaca() { 
+        return placa; }
+    public void setPlaca(String placa) { 
+        this.placa = placa; }
+
+    //MODELO
+    public String getModelo() { 
+        return modelo; }
+    public void setModelo(String modelo) { 
+        this.modelo = modelo; }
+
+    //MARCA
+    public String getMarca() { 
+        return marca; }
+    public void setMarca(String marca) { 
+        this.marca = marca; }
+
+    //ANO
+    public int getAno() { 
+        return ano; }
+    public void setAno(int ano) { 
+        this.ano = ano; }
+
+    //STATUS
+    public StatusVeiculo getStatus() { 
+        return status; }
+    public void setStatus(StatusVeiculo status) { 
+        this.status = status; }
+}
