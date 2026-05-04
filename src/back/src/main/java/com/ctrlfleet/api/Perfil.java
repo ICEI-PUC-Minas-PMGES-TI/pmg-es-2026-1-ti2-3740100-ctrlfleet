be6a7df413fe7425ecca0559cee7a192bb67beeb
RafaelGalileu <rafaeloliveirargto@gmail.com>
2026-05-03 22:44:43 -0300

    feat: Criação da classe Perfil

---
+++ src/back/src/main/java/com/ctrlfleet/api/Perfil.java
@@ -0,0 +1,40 @@
+package com.ctrlfleet.api;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Perfil {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoPerfil tipo;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public TipoPerfil getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPerfil tipo) {
+        this.tipo = tipo;
+    }
+    
+}
